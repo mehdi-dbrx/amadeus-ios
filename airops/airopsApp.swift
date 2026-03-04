965cbcf8a39d113dd2c1fc420d8b84b43fb84e07
Mehdi Lamrani <mehdi.lamrani@databricks.com>
2026-03-04 10:29:38 +0100

    Initial Commit

---
+++ airops/airopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  airopsApp.swift
+//  airops
+//
+//  Created by Mehdi Lamrani on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct airopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
